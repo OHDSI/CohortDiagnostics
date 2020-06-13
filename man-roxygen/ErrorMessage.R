d006b5f796d9ffe225cb9a0f4362b94c666f2ff7
Gowtham Rao <gowthamrao@users.noreply.github.com>
2020-06-12 21:16:00 -0400

    Delete ErrorMessage.R

--- man-roxygen/ErrorMessage.R
+++
@@ -1,4 +0,0 @@
-#' @param errorMessage        An object of type \code{“AssertCollection”} as created using the
-#'                            \code{\link[checkmate]{makeAssertCollection}} function in the
-#'                            checkmate package. If NULL, then new errorMessage object of 
-#'                            \code{“AssertCollection”} type is created to collect assertions.
