3223bec30b6a272704133a3d123bdc06a02b11aa
Gowtham Rao <gowthamrao@gmail.com>
2020-06-15 07:39:39 -0400

    Updated based on review feedback
    https://github.com/OHDSI/CohortDiagnostics/pull/76#discussion_r439948703

--- R/Private.R
+++ R/Private.R
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 createIfNotExist <- function(type, name , recursive = TRUE) {
   if (!is.null(type)) {
     if (length(name) == 0) {
