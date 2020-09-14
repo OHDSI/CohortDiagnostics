74cb0967eabc4baf2da014e9ca7158174ca1e48a
Gowtham Rao <gowthamrao@gmail.com>
2020-09-14 10:32:13 -0400

    Draft commit

---
+++ extras/tests/testResultsDataModel.R
@@ -0,0 +1,58 @@
+cohortId <- 141933001
+targetCohortIds <- cohortId
+comparatorCohortIds <- 141933003
+cohortIds <- c(targetCohortIds, comparatorCohortIds)
+databaseId <- 'OPTUM_EXTENDED_DOD'
+databaseIds <- c(databaseId, 'OPTUM_PANTHER')
+
+connection = NULL
+connectionDetails = NULL
+resultsDatabaseSchema = NULL
+library(magrittr)
+
+
+data <- CohortDiagnostics::getTimeDistributionResult(cohortIds = cohortIds, 
+                                                     databaseIds = databaseIds)
+CohortDiagnostics::plotTimeDistribution(data = data, 
+                                        cohortIds = c(141933001))
+
+
+data <- CohortDiagnostics::getIncidenceRateResult(cohortIds = cohortIds,
+                                                  stratifyByAgeGroup = TRUE,
+                                                  stratifyByGender = TRUE,
+                                                  stratifyByCalendarYear = TRUE,
+                                                  databaseIds = databaseIds)
+CohortDiagnostics::plotIncidenceRate(data = data,
+                                     stratifyByAgeGroup = TRUE,
+                                     stratifyByGender = TRUE,
+                                     stratifyByCalendarYear = TRUE)
+
+data <- CohortDiagnostics::getIncidenceRateResult(cohortIds = cohortIds,
+                                                  databaseIds = databaseIds,
+                                                  stratifyByGender = FALSE)
+
+data <- CohortDiagnostics::getCohortCountResult(databaseIds = databaseIds)
+
+data <- CohortDiagnostics::getCohortOverLapResult(targetCohortIds = targetCohortIds,
+                                                  comparatorCohortIds = comparatorCohortIds,
+                                                  databaseIds = databaseIds)
+
+covariateReference <- CohortDiagnostics::getCovariateReference(isTemporal = FALSE)
+temporalCovariateReference <- CohortDiagnostics::getCovariateReference(isTemporal = TRUE)
+timeRef <- CohortDiagnostics::getTimeReference()
+
+
+data <- CohortDiagnostics::getCovariateValueResult(cohortIds = cohortIds, 
+                                                   databaseIds = databaseIds, 
+                                                   minProportion = 0.03)
+
+data <- CohortDiagnostics::getCovariateValueResult(cohortIds = cohortIds, 
+                                                   databaseIds = databaseIds, 
+                                                   minProportion = 0.03,
+                                                   isTemporal = TRUE)
+
+data <- CohortDiagnostics::getCovariateValueResult(cohortIds = cohortIds, 
+                                                   databaseIds = databaseIds, 
+                                                   minProportion = 0.03,
+                                                   isTemporal = TRUE, 
+                                                   timeIds = c(1,2,3))
