22ae862394ba8cbc460d0258bf438036b0657cfe
Schuemie <MSCHUEMI@its.jnj.com>
2020-09-29 12:06:11 +0200

    Adding some progress output to code for fixing zip files

--- extras/FixZipFile.R
+++ extras/FixZipFile.R
@@ -1,7 +1,7 @@
 # Code for fixing old zip files in a folder
 
-oldDataFolder <- "s:/examplePhenotypeLibraryPackageOutput/CCAE/diagnosticsExport"
-newDataFolder <- "s:/examplePhenotypeLibraryPackageOutput/CCAE/diagnosticsExportFixed"
+oldDataFolder <- "C:/Users/MSCHUEMI/Desktop/Immunology"
+newDataFolder <- "C:/Users/MSCHUEMI/Desktop/ImmunologyFixed"
 
 # No changes beyond this point
 dir.create(newDataFolder, recursive = TRUE)
@@ -11,7 +11,10 @@ dir.create(path = unzipFolder, recursive = TRUE)
 
 zipFiles <- list.files(oldDataFolder, pattern = ".zip", full.names = TRUE, recursive = TRUE)
 for (zipFile in zipFiles) {
+  writeLines(paste("Unzipping", zipFile))
   zip::unzip(zipFile, exdir = unzipFolder)
+  
+  writeLines("Fixing cohort table")
   cohort <- readr::read_csv(file.path(unzipFolder, "cohort.csv"),
                             col_types = readr::cols(),
                             guess_max = 1e6)  
@@ -23,6 +26,8 @@ for (zipFile in zipFiles) {
     cohort$referent_concept_id <- NULL
   }
   readr::write_csv(cohort, file.path(unzipFolder, "cohort.csv"))
+  
+  writeLines("Zipping file")
   zip::zipr(file.path(newDataFolder, basename(zipFile)), list.files(unzipFolder, full.names = TRUE))
   unlink(list.files(unzipFolder, full.names = TRUE))  
 }
