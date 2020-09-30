a3467ff1731dce69f48f8e9a7908ab494de23186
schuemie <schuemie@ohdsi.org>
2020-09-30 09:49:46 -0400

    Using shiny server credentials in upload script

--- extras/UploadToDatabase.R
+++ extras/UploadToDatabase.R
@@ -1,10 +1,13 @@
 # Using the official uploading functions to get data from zip files into the postgres database
 library(CohortDiagnostics)
 connectionDetails <- createConnectionDetails(dbms = "postgresql",
-                                             server = "localhost/ohdsi",
-                                             user = "postgres",
-                                             password = Sys.getenv("pwPostgres"))
-schema <- "phenotype_library"
+                                             server = paste(Sys.getenv("phenotypeLibraryDbServer"),
+                                                            Sys.getenv("phenotypeLibraryDbDatabase"),
+                                                            sep = "/"),
+                                             port = Sys.getenv("phenotypeLibraryDbPort"),
+                                             user = Sys.getenv("phenotypeLibraryDbUser"),
+                                             password = Sys.getenv("phenotypeLibraryDbPassword"))
+schema <- Sys.getenv("phenotypeLibraryDbSchema")
 
 createResultsDataModel(connectionDetails = connectionDetails, schema = schema)
 uploadResults(connectionDetails = connectionDetails,
