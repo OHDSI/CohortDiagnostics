a32a3ccbd132b01154cb20d93048b9aa6aa2bc5a
jdide <jdi212@lehigh.edu>
2020-09-30 04:58:15 +0000

    variable cleanup

--- extras/PostgresConfigurationVariables.R
+++ extras/PostgresConfigurationVariables.R
@@ -1,10 +1,4 @@
-# Sys.setenv(server = "localhost/cohortDiagnosticsDB")
-# Sys.setenv(port = 5432)
-# Sys.setenv(user = "postgres")
-# Sys.setenv(password = "1234")
-
-
-Sys.setenv(PL_DB_SERVER = "localhost/postgres")
-Sys.setenv(PL_DB_PORT = 5432)
-Sys.setenv(PL_DB_USER = "postgres")
-Sys.setenv(PL_DB_PWD = "r00t_passw0rd")
+Sys.setenv(server = "localhost/cohortDiagnosticsDB")
+Sys.setenv(port = 5432)
+Sys.setenv(user = "postgres")
+Sys.setenv(password = "1234")
