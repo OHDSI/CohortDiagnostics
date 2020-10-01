faa0e847bacb9d6aa55200b0cd69b1c111c4a0f5
Gowtham Rao <gowthamrao@gmail.com>
2020-10-01 04:44:43 -0400

    Commented code
    Adding some commented out code that was used for testing purposes

--- inst/shiny/DiagnosticsExplorer/R/Other.R
+++ inst/shiny/DiagnosticsExplorer/R/Other.R
@@ -5,3 +5,37 @@ phenotypeLibraryModeDefaultTitle <- "Phenotype Library"
 
 thresholdCohortSubjects <- 0
 thresholdCohortEntries <- 0
+
+
+
+
+#####################
+# if (!is.null(Sys.getenv('user')) &&
+#     !is.null(Sys.getenv('phenotypeLibraryDbServer'))) {
+#   connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "postgresql",
+#                                                                   server = paste(Sys.getenv("phenotypeLibraryDbServer"),
+#                                                                                  Sys.getenv("phenotypeLibraryDbDatabase"),
+#                                                                                  sep = "/"),
+#                                                                   port = Sys.getenv("phenotypeLibraryDbPort"),
+#                                                                   user = Sys.getenv("phenotypeLibraryDbUser"),
+#                                                                   password = Sys.getenv("phenotypeLibraryDbPassword"))
+#   connection <- DatabaseConnector::connect(connectionDetails = connectionDetails)
+#   resultsDatabaseSchema <- Sys.getenv('phenotypeLibraryResultsDbSchema')
+#   vocabularyDatabaseSchema <- Sys.getenv('phenotypeLibraryVocabularyDbSchema')
+# } else {
+#   connection <- NULL
+# }
+# 
+# queryAllData <- function(connection,
+#                          databaseSchema, 
+#                          tableName) {
+#   sql <- "SELECT * FROM @database_schema.@table_name"
+#   data <- DatabaseConnector::renderTranslateQuerySql(connection = connection,
+#                                                      sql = sql,
+#                                                      snakeCaseToCamelCase = TRUE,
+#                                                      database_schema = databaseSchema,
+#                                                      table_name = tableName
+#   ) %>% 
+#     dplyr::tibble()
+#   return(data)
+# }
--- inst/shiny/DiagnosticsExplorer/global.R
+++ inst/shiny/DiagnosticsExplorer/global.R
@@ -63,3 +63,40 @@ if (exists("temporalTimeRef")) {
     dplyr::arrange(.data$timeId) %>% 
     dplyr::slice_head(n = 5)
 }
+
+
+
+# 
+# if (is.null(connection)) {
+#   if (!exists("shinySettings")) {
+#     if (file.exists("data")) {
+#       shinySettings <- list(dataFolder = "data")
+#     } else {
+#       shinySettings <- list(dataFolder = "S:/examplePackageOutput")
+#     }
+#     dataFolder <- shinySettings$dataFolder
+#     if (file.exists(file.path(dataFolder, "PreMerged.RData"))) {
+#       writeLines(paste0("Using merged data detected in folder '", dataFolder, "'"))
+#       load(file.path(dataFolder, "PreMerged.RData"))
+#     } else {
+#       stop("No premerged file found")
+#     }
+#   }
+# } else {
+#   writeLines(paste0("Retrieving some tables from databse "))
+#   allTables <- DatabaseConnector::getTableNames(connection = connection,
+#                                                 databaseSchema = resultsDatabaseSchema)
+#   globalTables <- c('analysis_ref', 'temporal_time_ref', 'cohort', 'cohort_count',
+#                     'concept_sets', 'database', 'phenotype_description')
+#   for (i in (1:length(globalTables))) {
+#     assign(SqlRender::snakeCaseToCamelCase(globalTables[[i]]), 
+#            queryAllData(connection = connection,
+#                         databaseSchema = resultsDatabaseSchema,
+#                         tableName = globalTables[[i]]))
+#   }
+#   blankTables <- setdiff(x = allTables, y = globalTables)
+#   for (i in (1:length(blankTables))) {
+#     assign(SqlRender::snakeCaseToCamelCase(blankTables[[i]]),
+#            tidyr::tibble())
+#   }
+# }
