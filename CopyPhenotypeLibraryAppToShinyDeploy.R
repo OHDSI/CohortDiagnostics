238312b00f15461c5c21e6fe6fe21e4ec450873a
schuemie <schuemie@ohdsi.org>
2020-10-15 04:54:21 -0400

    Adding script for deploying phenotype library app to OHDSI Shiny server

---
+++ CopyPhenotypeLibraryAppToShinyDeploy.R
@@ -0,0 +1,24 @@
+# Code for copying the Shiny app to the ShinyDeploy repo
+# Assumes you have cloned both repos, that the current working directory is the root of CohortDiagnostics, and that 
+# the ShinyDeploy folder is a sibling of CohortDiagnostics.
+
+sourceFolder <- normalizePath("inst/shiny/DiagnosticsExplorer")
+targetFolder <- normalizePath("../ShinyDeploy/PhenotypeLibrary")
+ 
+# Copy app -------------------------------------------------------------------------
+if (file.exists(targetFolder)) {  
+  unlink(targetFolder, recursive = TRUE)
+}
+R.utils::copyDirectory(sourceFolder, targetFolder)
+
+# Modify global.R ------------------------------------------------------------------
+global <- SqlRender::readSql(file.path(targetFolder, "global.R"))
+
+# Change about text
+aboutText <- "
+<h3>The OHDSI Phenotype Library</h3>
+This app is under development. Please do not use.
+"
+global <- gsub("defaultAboutText <-.+?\r\n", sprintf("defaultAboutText <- \"%s\"\r\n", aboutText), global)
+
+SqlRender::writeSql(global, file.path(targetFolder, "global.R"))
