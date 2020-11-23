1a04f2427fcf7a3ea89ee902379c58a4a24129ad
Gowtham Rao <rao@ohdsi.org>
2020-11-23 05:40:07 -0500

    databaseName and databaseDescription default values should be non-NULL
    Fixes issue https://github.com/OHDSI/CohortDiagnostics/issues/301
    
    * updated description file and news file for v2.1.0

--- extras/runOhdsiShinyServer.R
+++ extras/runOhdsiShinyServer.R
@@ -6,11 +6,3 @@ connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "postgres
                                                                 user = Sys.getenv("phoebedbUser"),
                                                                 password = Sys.getenv("phoebedbPw"))
 
-CohortDiagnostics::launchDiagnosticsExplorer(connectionDetails = connectionDetails, 
-                                             resultsDatabaseSchema = Sys.getenv("phoebedbTargetSchema"), 
-                                             vocabularyDatabaseSchema = Sys.getenv("phoebedbVocabSchema"), 
-                                             cohortBaseUrl = "https://epi.jnj.com/atlas/#/cohortdefinition/", 
-                                             conceptBaseUrl = "https://epi.jnj.com/atlas/#/concept/", 
-                                             runOverNetwork = TRUE,
-                                             aboutText = 'TRUE'
-)
