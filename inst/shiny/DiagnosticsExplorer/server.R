12ef01ae588126435d83367f0bd087450346641c
Gowtham Rao <gowthamrao@gmail.com>
2021-01-28 07:17:24 -0500

    Removed hidden search using html - as it causes error when using complex search terms. Will input selectize input instead

--- inst/shiny/DiagnosticsExplorer/server.R
+++ inst/shiny/DiagnosticsExplorer/server.R
@@ -93,15 +93,8 @@ shiny::shinyServer(function(input, output, session) {
   # Phenotype Description ------------------------------------------------------------------------------
   output$phenoTypeDescriptionTable <- DT::renderDataTable(expr = {
     data <- phenotypeDescription %>%
-      dplyr::mutate(
-        name = sprintf(
-          "%s <div style=\"display: none\">%s</div>",
-          .data$phenotypeName,
-          .data$searchTermString
-        )
-      ) %>%
       dplyr::select(.data$phenotypeId,
-                    .data$name,
+                    .data$phenotypeName,
                     .data$overview,
                     .data$cohortDefinitions)
     
