a2f0ef58c8798b7ee612d080eef6e0b79e015a44
Gowtham Rao <gowthamrao@gmail.com>
2021-03-15 15:04:57 -0400

    Add subject_count to index event breakdown

--- inst/sql/sql_server/CohortEntryBreakdown.sql
+++ inst/sql/sql_server/CohortEntryBreakdown.sql
@@ -16,7 +16,8 @@ INTO @store_table
 }
 FROM (
 	SELECT @domain_concept_id AS concept_id,
-		COUNT(*) AS concept_count
+		COUNT(*) AS concept_count,
+		COUNT(distinct subject_count) AS subject_count
 	FROM @cohort_database_schema.@cohort_table
 	INNER JOIN @cdm_database_schema.@domain_table
 		ON subject_id = person_id
