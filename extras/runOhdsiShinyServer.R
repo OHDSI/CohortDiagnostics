5e3bc7952e14800422722e04062bc86924e105a3
Gowtham Rao <gowthamrao@gmail.com>
2021-03-18 22:21:41 -0400

    removed unnecessary files

--- extras/runOhdsiShinyServer.R
+++
@@ -1,8 +0,0 @@
-connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "postgresql",
-                                                                server = paste(Sys.getenv("phoebedbServer"),
-                                                                               Sys.getenv("phoebedb"),
-                                                                               sep = "/"),
-                                                                port = Sys.getenv("phenotypeLibraryDbPort"),
-                                                                user = Sys.getenv("phoebedbUser"),
-                                                                password = Sys.getenv("phoebedbPw"))
-
