12913ff426ba7f8ea05c2bda91491c6f4edc2d03
Gowtham Rao <gowthamrao@gmail.com>
2021-03-31 00:21:36 +0530

    Removed lasso from temporal characterization

--- inst/shiny/DiagnosticsExplorer/ui.R
+++ inst/shiny/DiagnosticsExplorer/ui.R
@@ -469,78 +469,7 @@ bodyTabItems <- shinydashboard::tabItems(
   shinydashboard::tabItem(
     tabName = "temporalCharacterization",
     cohortReference("temporalCharacterizationSelectedCohort"),
-    shiny::radioButtons(
-      inputId = "tempCharType",
-      label = "",
-      choices = c("Table", "Plot"),
-      selected = "Table",
-      inline = TRUE
-    ),
-    shiny::conditionalPanel(condition = "input.tempCharType=='Table'",
-                            DT::dataTableOutput("temporalCharacterizationTable")),
-    shiny::conditionalPanel(
-      condition = "input.tempCharType=='Plot'",
-  
-      tags$table(style = "width:100%",
-                 tags$tr(
-                   tags$td(
-                     shinyWidgets::pickerInput(
-                       inputId = "timeIdChoicesFilter",
-                       label = "Filter By Temporal Choices",
-                       choices = c("All", temporalCovariateChoices$choices),
-                       multiple = FALSE,
-                       choicesOpt = list(style = rep_len("color: black;", 999)),
-                       options = shinyWidgets::pickerOptions(
-                         actionsBox = TRUE,
-                         liveSearch = TRUE,
-                         size = 10,
-                         liveSearchStyle = "contains",
-                         liveSearchPlaceholder = "Type here to search",
-                         virtualScroll = 50))
-                   ),
-                   tags$td(
-                     shinyWidgets::pickerInput(
-                       inputId = "temporalDomainId",
-                       label = "Filter By Covariate Domain",
-                       choices = c("all","condition", "device", "drug", "measurement", "observation", "procedure", "other"),
-                       multiple = FALSE,
-                       choicesOpt = list(style = rep_len("color: black;", 999)),
-                       options = shinyWidgets::pickerOptions(
-                         actionsBox = TRUE, 
-                         liveSearch = TRUE, 
-                         size = 10,
-                         liveSearchStyle = 'contains',
-                         liveSearchPlaceholder = "Type here to search",
-                         virtualScroll = 50))
-                   )
-                 )
-      ),
-      shinydashboard::box(
-        title = "Compare Temporal Characterization",
-        width = NULL,
-        status = "primary",
-        fluidPage(
-          fluidRow(
-            column(3,DT::dataTableOutput("temporalCharacterizationCovariateTable")),
-            column(9,ggiraph::ggiraphOutput("compareTemporalCharacterizationPlot", width = "100%", height = "100%"))
-          )
-        )
-      ),
-      shiny::conditionalPanel(
-        condition = "input.compareTemporalCharacterizationPlot_selected.length>0",
-        shinydashboard::box(
-          title = "Selected covariates",
-          width = NULL,
-          status = "primary",
-          fluidPage(
-            fluidRow(
-              column(6,DT::dataTableOutput("temporalCharacterizationCovariateLassoTable")),
-              column(6,ggiraph::ggiraphOutput("compareTemporalCharacterizationLassoPlot", width = "100%", height = "100%"))
-            )
-          )
-        )
-        )
-    )
+    DT::dataTableOutput("temporalCharacterizationTable")
   ),
   shinydashboard::tabItem(
     tabName = "cohortOverlap",
