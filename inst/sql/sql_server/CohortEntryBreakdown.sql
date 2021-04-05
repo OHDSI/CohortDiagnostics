81b560cd6258c6fcb108e802cad45e30c0b70ad8
Gowtham Rao <gowthamrao@gmail.com>
2021-04-05 13:41:44 -0400

    Fix for issue related to index event breakdown
    https://github.com/OHDSI/CohortDiagnostics/issues/331

--- inst/sql/sql_server/CohortEntryBreakdown.sql
+++ inst/sql/sql_server/CohortEntryBreakdown.sql
@@ -1,12 +1,15 @@
 {DEFAULT @store = FALSE}
 {DEFAULT @store_table = #breakdown}
+{DEFAULT @use_source_concept_id = FALSE}
 
 {@store} ? {
 IF OBJECT_ID('tempdb..@store_table', 'U') IS NOT NULL
   DROP TABLE @store_table;
 }
 
-SELECT concept.concept_id,
+SELECT domain_table,
+  domain_field,
+  concept.concept_id,
 	concept_count,
 	subject_count
 {@store} ? {
@@ -15,7 +18,9 @@ INTO @store_table
 	,concept_name
 }
 FROM (
-	SELECT @domain_concept_id AS concept_id,
+	SELECT "@domain_table" AS domain_table,
+	  "@domain_concept_id" AS domain_field,
+	  @domain_concept_id AS concept_id,
 		COUNT(*) AS concept_count,
 		COUNT(distinct subject_id) AS subject_count
 	FROM @cohort_database_schema.@cohort_table
@@ -28,9 +33,12 @@ FROM (
 		AND codeset_id IN (@primary_codeset_ids)
 	GROUP BY @domain_concept_id
 	
+	{use_source_concept_id} ? {
 	UNION
 	
-	SELECT @domain_source_concept_id AS concept_id,
+	SELECT "@domain_table" AS domain_table,
+	  "@domain_concept_id" AS domain_field,
+	  @domain_source_concept_id AS concept_id,
 		COUNT(*) AS concept_count,
 		COUNT(distinct subject_id) AS subject_count
 	FROM @cohort_database_schema.@cohort_table
@@ -45,7 +53,9 @@ FROM (
 	
 	UNION
 	
-	SELECT @domain_source_concept_id AS concept_id,
+	SELECT "@domain_table" AS domain_table,
+	  "@domain_concept_id" AS domain_field,
+	  @domain_source_concept_id AS concept_id,
 		COUNT(*) AS concept_count,
 		COUNT(distinct subject_id) AS subject_count
 	FROM @cohort_database_schema.@cohort_table
@@ -57,6 +67,7 @@ FROM (
 	WHERE cohort_definition_id = @cohort_id
 		AND codeset_id IN (@primary_codeset_ids)
 	GROUP BY @domain_source_concept_id
+	}
 	
 	) concept_counts
 INNER JOIN @cdm_database_schema.concept
