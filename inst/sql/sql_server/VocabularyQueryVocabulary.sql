db568cbe6a35bedb9fc8e4d0ca959b7f2e80cf3c
Gowtham Rao <gowthamrao@gmail.com>
2021-04-12 11:12:52 -0400

    Deleting unnecessary file

--- inst/sql/sql_server/VocabularyQueryVocabulary.sql
+++
@@ -1,5 +0,0 @@
-SELECT distinct vocabulary.*
-  FROM @vocabulary_database_schema.concept
-INNER JOIN @vocabulary_database_schema.vocabulary
-ON concept.vocabulary_id = vocabulary.vocabulary_id
-where concept.concept_id in (@conceptIds);
