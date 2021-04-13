654c40b838a040c96b9049c2c205d067360af0e2
Gowtham Rao <gowthamrao@gmail.com>
2021-04-13 07:15:44 -0400

    Formatting test that script

--- tests/testthat/test-againstEunomia.R
+++ tests/testthat/test-againstEunomia.R
@@ -3,87 +3,103 @@ library(testthat)
 library(CohortDiagnostics)
 library(Eunomia)
 
-baseUrl <- Sys.getenv("OHDSIbaseUrl")
-
 connectionDetails <- Eunomia::getEunomiaConnectionDetails()
 cdmDatabaseSchema <- "main"
-vocabularyDatabaseSchema <- cohortDatabaseSchema
 cohortDatabaseSchema <- "main"
+vocabularyDatabaseSchema <- cohortDatabaseSchema
 cohortTable <- "cohort"
 oracleTempSchema <- NULL
 folder <- tempfile()
 dir.create(folder, recursive = TRUE)
 
 test_that("Cohort instantiation", {
-  CohortDiagnostics::instantiateCohortSet(connectionDetails = connectionDetails,
-                                          cdmDatabaseSchema = cdmDatabaseSchema,
-                                          vocabularyDatabaseSchema = vocabularyDatabaseSchema,
-                                          oracleTempSchema = oracleTempSchema,
-                                          cohortDatabaseSchema = cohortDatabaseSchema,
-                                          cohortTable = cohortTable,
-                                          packageName = "CohortDiagnostics",
-                                          cohortToCreateFile = "settings/CohortsToCreateForTesting.csv",
-                                          generateInclusionStats = TRUE,
-                                          createCohortTable = TRUE,
-                                          inclusionStatisticsFolder = file.path(folder, "incStats"))
+  CohortDiagnostics::instantiateCohortSet(
+    connectionDetails = connectionDetails,
+    cdmDatabaseSchema = cdmDatabaseSchema,
+    vocabularyDatabaseSchema = vocabularyDatabaseSchema,
+    oracleTempSchema = oracleTempSchema,
+    cohortDatabaseSchema = cohortDatabaseSchema,
+    cohortTable = cohortTable,
+    packageName = "CohortDiagnostics",
+    cohortToCreateFile = "settings/CohortsToCreateForTesting.csv",
+    generateInclusionStats = TRUE,
+    createCohortTable = TRUE,
+    inclusionStatisticsFolder = file.path(folder, "incStats")
+  )
   
   
   connection <- DatabaseConnector::connect(connectionDetails)
-  sql <- "SELECT COUNT(*) AS cohort_count, cohort_definition_id FROM @cohort_database_schema.@cohort_table GROUP BY cohort_definition_id;"
-  counts <- DatabaseConnector::renderTranslateQuerySql(connection, sql, cohort_database_schema = cohortDatabaseSchema, cohort_table = cohortTable, snakeCaseToCamelCase = TRUE)
+  sql <-
+    "SELECT COUNT(*) AS cohort_count, cohort_definition_id 
+  FROM @cohort_database_schema.@cohort_table 
+  GROUP BY cohort_definition_id;"
+  counts <-
+    DatabaseConnector::renderTranslateQuerySql(
+      connection,
+      sql,
+      cohort_database_schema = cohortDatabaseSchema,
+      cohort_table = cohortTable,
+      snakeCaseToCamelCase = TRUE
+    )
   testthat::expect_gt(nrow(counts), 2)
   DatabaseConnector::disconnect(connection)
 })
 
 test_that("Cohort diagnostics in incremental mode", {
   firstTime <- system.time(
-    CohortDiagnostics::runCohortDiagnostics(connectionDetails = connectionDetails,
-                                            cdmDatabaseSchema = cdmDatabaseSchema,
-                                            vocabularyDatabaseSchema = vocabularyDatabaseSchema,
-                                            oracleTempSchema = oracleTempSchema,
-                                            cohortDatabaseSchema = cohortDatabaseSchema,
-                                            cohortTable = cohortTable,
-                                            packageName = "CohortDiagnostics",
-                                            cohortToCreateFile = "settings/CohortsToCreateForTesting.csv",
-                                            inclusionStatisticsFolder = file.path(folder, "incStats"),
-                                            exportFolder =  file.path(folder, "export"),
-                                            databaseId = "Eunomia",
-                                            runInclusionStatistics = TRUE,
-                                            runBreakdownIndexEvents = TRUE,
-                                            runCohortCharacterization = TRUE,
-                                            runTemporalCohortCharacterization = TRUE,
-                                            runCohortOverlap = TRUE,
-                                            runIncidenceRate = TRUE,
-                                            runIncludedSourceConcepts = TRUE,
-                                            runOrphanConcepts = TRUE,
-                                            runTimeDistributions = TRUE,
-                                            incremental = TRUE,
-                                            incrementalFolder = file.path(folder, "incremental"))
+    CohortDiagnostics::runCohortDiagnostics(
+      connectionDetails = connectionDetails,
+      cdmDatabaseSchema = cdmDatabaseSchema,
+      vocabularyDatabaseSchema = vocabularyDatabaseSchema,
+      oracleTempSchema = oracleTempSchema,
+      cohortDatabaseSchema = cohortDatabaseSchema,
+      cohortTable = cohortTable,
+      packageName = "CohortDiagnostics",
+      cohortToCreateFile = "settings/CohortsToCreateForTesting.csv",
+      inclusionStatisticsFolder = file.path(folder, "incStats"),
+      exportFolder =  file.path(folder, "export"),
+      databaseId = "Eunomia",
+      runInclusionStatistics = TRUE,
+      runBreakdownIndexEvents = TRUE,
+      runCohortCharacterization = TRUE,
+      runTemporalCohortCharacterization = TRUE,
+      runCohortOverlap = TRUE,
+      runIncidenceRate = TRUE,
+      runIncludedSourceConcepts = TRUE,
+      runOrphanConcepts = TRUE,
+      runTimeDistributions = TRUE,
+      incremental = TRUE,
+      incrementalFolder = file.path(folder, "incremental")
+    )
   )
   
-  testthat::expect_true(file.exists(file.path(folder, "export", "Results_Eunomia.zip")))
+  testthat::expect_true(file.exists(file.path(
+    folder, "export", "Results_Eunomia.zip"
+  )))
   
   secondTime <- system.time(
-    CohortDiagnostics::runCohortDiagnostics(connectionDetails = connectionDetails,
-                                            cdmDatabaseSchema = cdmDatabaseSchema,
-                                            oracleTempSchema = oracleTempSchema,
-                                            cohortDatabaseSchema = cohortDatabaseSchema,
-                                            cohortTable = cohortTable,
-                                            packageName = "CohortDiagnostics",
-                                            cohortToCreateFile = "settings/CohortsToCreateForTesting.csv",
-                                            inclusionStatisticsFolder = file.path(folder, "incStats"),
-                                            exportFolder =  file.path(folder, "export"),
-                                            databaseId = "Eunomia",
-                                            runInclusionStatistics = TRUE,
-                                            runBreakdownIndexEvents = TRUE,
-                                            runCohortCharacterization = TRUE,
-                                            runCohortOverlap = TRUE,
-                                            runIncidenceRate = TRUE,
-                                            runIncludedSourceConcepts = TRUE,
-                                            runOrphanConcepts = TRUE,
-                                            runTimeDistributions = TRUE,
-                                            incremental = TRUE,
-                                            incrementalFolder = file.path(folder, "incremental"))
+    CohortDiagnostics::runCohortDiagnostics(
+      connectionDetails = connectionDetails,
+      cdmDatabaseSchema = cdmDatabaseSchema,
+      oracleTempSchema = oracleTempSchema,
+      cohortDatabaseSchema = cohortDatabaseSchema,
+      cohortTable = cohortTable,
+      packageName = "CohortDiagnostics",
+      cohortToCreateFile = "settings/CohortsToCreateForTesting.csv",
+      inclusionStatisticsFolder = file.path(folder, "incStats"),
+      exportFolder =  file.path(folder, "export"),
+      databaseId = "Eunomia",
+      runInclusionStatistics = TRUE,
+      runBreakdownIndexEvents = TRUE,
+      runCohortCharacterization = TRUE,
+      runCohortOverlap = TRUE,
+      runIncidenceRate = TRUE,
+      runIncludedSourceConcepts = TRUE,
+      runOrphanConcepts = TRUE,
+      runTimeDistributions = TRUE,
+      incremental = TRUE,
+      incrementalFolder = file.path(folder, "incremental")
+    )
   )
   testthat::expect_lt(secondTime[1], firstTime[1])
 })
