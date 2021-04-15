291431a70ad130fcc945ccba029433ad022f9548
Gowtham Rao <gowthamrao@gmail.com>
2021-04-15 07:01:20 -0400

    Adding test that other

---
+++ tests/testthat/test-other.R
@@ -0,0 +1,5 @@
+library(testthat)
+test_that("Check if package is installed", {
+  expect_true(CohortDiagnostics:::is_installed('dplyr'))
+  expect_false(CohortDiagnostics:::is_installed('Hydra'))
+})
