611609d5c7dd63f7beb5de39c37698690c9d9fa5
Gowtham Rao <rao@ohdsi.org>
2021-05-11 19:49:39 -0400

    add join

--- inst/shiny/DiagnosticsExplorer/server.R
+++ inst/shiny/DiagnosticsExplorer/server.R
@@ -1691,7 +1691,7 @@ shiny::shinyServer(function(input, output, session) {
     databaseIds <- unique(data$databaseId)
     
     cohortCounts <- data %>% 
-      dplyr::inner_join(cohortCount) %>% 
+      dplyr::inner_join(y = cohortCount, by = c("cohortId", "databaseId")) %>% 
       dplyr::filter(.data$cohortId == cohortId()) %>% 
       dplyr::filter(.data$databaseId %in% databaseIds()) %>% 
       dplyr::select(.data$cohortSubjects) %>% 
