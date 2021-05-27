e4623fe4780265f0dfb21a17f0612294779ef69e
Gowtham Rao <rao@ohdsi.org>
2021-05-27 06:05:32 -0400

    optimized sql
    https://github.com/OHDSI/CohortDiagnostics/issues/420

--- inst/sql/sql_server/CohortOverlap.sql
+++ inst/sql/sql_server/CohortOverlap.sql
@@ -1,9 +1,11 @@
-{DEFAULT @cohort_database_schema = cdm_optum_extended_dod_v1027.ohdsi_results}
-{DEFAULT @cohort_table = cohort}
-{DEFAULT @target_cohort_id = 10481}
-{DEFAULT @comparator_cohort_id = 12770}
+{DEFAULT @cohort_database_schema = cdm_optum_extended_dod_v1027.ohdsi_results} 
+{DEFAULT @cohort_table = cohort} 
+{DEFAULT @target_cohort_id = 10481 } 
+{DEFAULT @comparator_cohort_id = 12770 }
 
-SELECT union_counts.num_persons_in_either AS either_subjects,
+SELECT target_cohort_id,
+	comparator_cohort_id,
+	union_counts.num_persons_in_either AS either_subjects,
 	union_counts.num_persons_in_both AS both_subjects,
 	union_counts.num_persons_in_t_only AS t_only_subjects,
 	union_counts.num_persons_in_c_only AS c_only_subjects,
@@ -13,7 +15,9 @@ SELECT union_counts.num_persons_in_either AS either_subjects,
 	intersection_counts.num_persons_t_in_c AS t_in_c_subjects,
 	intersection_counts.num_persons_c_in_t AS c_in_t_subjects
 FROM (
-	SELECT COUNT(all_persons.subject_id) AS num_persons_in_either,
+	SELECT target_cohort_id,
+		comparator_cohort_id,
+		COUNT(all_persons.subject_id) AS num_persons_in_either, --this is unique persons
 		SUM(CASE 
 				WHEN t1.subject_id IS NOT NULL
 					AND c1.subject_id IS NOT NULL
@@ -35,26 +39,28 @@ FROM (
 	FROM (
 		SELECT DISTINCT subject_id
 		FROM @cohort_database_schema.@cohort_table
-		WHERE cohort_definition_id IN (
-				@target_cohort_id,
-				@comparator_cohort_id
-				)
+		WHERE cohort_definition_id IN (@target_cohort_id)
+			OR cohort_definition_id IN (@comparator_cohort_id)
 		) all_persons
 	LEFT JOIN (
-		SELECT DISTINCT subject_id
+		SELECT DISTINCT cohort_definition_id target_cohort_id,
+			subject_id
 		FROM @cohort_database_schema.@cohort_table
-		WHERE cohort_definition_id = @target_cohort_id
-		) t1
-		ON all_persons.subject_id = t1.subject_id
+		WHERE cohort_definition_id IN (@target_cohort_id)
+		) t1 ON all_persons.subject_id = t1.subject_id
 	LEFT JOIN (
-		SELECT DISTINCT subject_id
+		SELECT DISTINCT cohort_definition_id comparator_cohort_id,
+			subject_id
 		FROM @cohort_database_schema.@cohort_table
 		WHERE cohort_definition_id = @comparator_cohort_id
-		) c1
-		ON all_persons.subject_id = c1.subject_id
+		) c1 ON all_persons.subject_id = c1.subject_id
+	GROUP BY target_cohort_id,
+		comparator_cohort_id
 	) union_counts,
 	(
-		SELECT SUM(CASE 
+		SELECT target_cohort_id,
+			comparator_cohort_id,
+			SUM(CASE 
 					WHEN t1.min_start < c1.min_start
 						THEN 1
 					ELSE 0
@@ -82,20 +88,26 @@ FROM (
 					ELSE 0
 					END) AS num_persons_c_in_t
 		FROM (
-			SELECT subject_id,
+			SELECT cohort_definition_id target_cohort_id,
+				subject_id,
 				MIN(cohort_start_date) AS min_start,
 				MIN(cohort_end_date) AS min_end
 			FROM @cohort_database_schema.@cohort_table
 			WHERE cohort_definition_id = @target_cohort_id
-			GROUP BY subject_id
+			GROUP BY cohort_definition_id,
+				subject_id
 			) t1
 		INNER JOIN (
-			SELECT subject_id,
+			SELECT cohort_definition_id comparator_cohort_id,
+				subject_id,
 				MIN(cohort_start_date) AS min_start,
 				MIN(cohort_end_date) AS min_end
 			FROM @cohort_database_schema.@cohort_table
 			WHERE cohort_definition_id = @comparator_cohort_id
-			GROUP BY subject_id
-			) c1
-			ON t1.subject_id = c1.subject_id
+			GROUP BY cohort_definition_id,
+				subject_id
+			) c1 ON t1.target_cohort_id = c1.comparator_cohort_id
+			AND t1.subject_id = c1.subject_id
+		GROUP BY target_cohort_id,
+			comparator_cohort_id
 		) intersection_counts;
