9bd297451d797637ac6ca19786bfc7975a84a477
Gowtham Rao <rao@ohdsi.org>
2021-05-27 06:21:51 -0400

    Added batching to cohort overlap query

--- R/CohortComparisonDiagnostics.R
+++ R/CohortComparisonDiagnostics.R
@@ -18,8 +18,9 @@ computeCohortOverlap <- function(connectionDetails = NULL,
                                  connection = NULL,
                                  cohortDatabaseSchema,
                                  cohortTable = "cohort",
-                                 targetCohortId,
-                                 comparatorCohortId) {
+                                 targetCohortIds,
+                                 comparatorCohortIds,
+                                 batchSize = 200) {
   start <- Sys.time()
   
   if (is.null(connection)) {
@@ -27,66 +28,87 @@ computeCohortOverlap <- function(connectionDetails = NULL,
     on.exit(DatabaseConnector::disconnect(connection))
   }
   
-  if (!checkIfCohortInstantiated(
-    connection = connection,
-    cohortDatabaseSchema = cohortDatabaseSchema,
-    cohortTable = cohortTable,
-    cohortId = targetCohortId
-  )) {
-    warning(
-      "- Target cohort with ID ",
-      targetCohortId,
-      " appears to be empty. Was it instantiated? Skipping overlap computation."
-    )
-    delta <- Sys.time() - start
-    ParallelLogger::logInfo(paste(
-      "Computing overlap took",
-      signif(delta, 3),
-      attr(delta, "units")
-    ))
-    return(tidyr::tibble())
-  }
+  ## commenting out checks if cohort is instantiated, as this is not essential.
+  ## if a cohort has 0 records, the overlap query will return NULL
+  # if (!checkIfCohortInstantiated(
+  #   connection = connection,
+  #   cohortDatabaseSchema = cohortDatabaseSchema,
+  #   cohortTable = cohortTable,
+  #   cohortId = targetCohortId
+  # )) {
+  #   warning(
+  #     "- Target cohort with ID ",
+  #     targetCohortId,
+  #     " appears to be empty. Was it instantiated? Skipping overlap computation."
+  #   )
+  #   delta <- Sys.time() - start
+  #   ParallelLogger::logInfo(paste(
+  #     "Computing overlap took",
+  #     signif(delta, 3),
+  #     attr(delta, "units")
+  #   ))
+  #   return(tidyr::tibble())
+  # }
   
-  if (!checkIfCohortInstantiated(
-    connection = connection,
-    cohortDatabaseSchema = cohortDatabaseSchema,
-    cohortTable = cohortTable,
-    cohortId = comparatorCohortId
-  )) {
-    warning(
-      "- Comparator cohort with ID ",
-      comparatorCohortId,
-      " appears to be empty. Was it instantiated? Skipping overlap computation."
-    )
-    delta <- Sys.time() - start
-    ParallelLogger::logInfo(paste(
-      "Computing overlap took",
-      signif(delta, 3),
-      attr(delta, "units")
-    ))
-    return(tidyr::tibble())
-  }
+  # if (!checkIfCohortInstantiated(
+  #   connection = connection,
+  #   cohortDatabaseSchema = cohortDatabaseSchema,
+  #   cohortTable = cohortTable,
+  #   cohortId = comparatorCohortId
+  # )) {
+  #   warning(
+  #     "- Comparator cohort with ID ",
+  #     comparatorCohortId,
+  #     " appears to be empty. Was it instantiated? Skipping overlap computation."
+  #   )
+  #   delta <- Sys.time() - start
+  #   ParallelLogger::logInfo(paste(
+  #     "Computing overlap took",
+  #     signif(delta, 3),
+  #     attr(delta, "units")
+  #   ))
+  #   return(tidyr::tibble())
+  # }
   
-  sql <- SqlRender::loadRenderTranslateSql(
-    "CohortOverlap.sql",
-    packageName = "CohortDiagnostics",
-    dbms = connection@dbms,
-    cohort_database_schema = cohortDatabaseSchema,
-    cohort_table = cohortTable,
-    target_cohort_id = targetCohortId,
-    comparator_cohort_id = comparatorCohortId
-  )
-  overlap <- DatabaseConnector::querySql(
-    connection = connection,
-    sql = sql,
-    snakeCaseToCamelCase = TRUE
-  ) %>%
-    tidyr::tibble()
+  cohortIds <- c(targetCohortIds, comparatorCohortIds) %>% unique() %>% arrange()
+  
+  results <- Andromeda::andromeda()
+  for (start in seq(1, length(cohortIds), by = batchSize)) {
+    end <- min(start + batchSize - 1, length(cohortIds))
+    if (length(cohortIds) > batchSize) {
+      ParallelLogger::logInfo(sprintf(
+        "Batch characterization. Processing cohorts %s through %s",
+        start,
+        end
+      ))
+    }
+    
+    sql <- SqlRender::loadRenderTranslateSql(
+      "CohortOverlap.sql",
+      packageName = "CohortDiagnostics",
+      dbms = connection@dbms,
+      cohort_database_schema = cohortDatabaseSchema,
+      cohort_table = cohortTable,
+      target_cohort_id = targetCohortId,
+      comparator_cohort_id = comparatorCohortId
+    )
+    overlap <- DatabaseConnector::querySql(
+      connection = connection,
+      sql = sql,
+      snakeCaseToCamelCase = TRUE
+    )
+    if ("overlap" %in% names(results)) {
+      Andromeda::appendToTable(results$overlap, overlap)
+    } else {
+      results$overlap <- overlap
+    }
+  }
+  overlapAll <- results$overlap %>% dplyr::collect()
   delta <- Sys.time() - start
   ParallelLogger::logInfo(paste(
     "Computing overlap took",
     signif(delta, 3),
     attr(delta, "units")
   ))
-  return(overlap)
+  return(overlapAll)
 }
