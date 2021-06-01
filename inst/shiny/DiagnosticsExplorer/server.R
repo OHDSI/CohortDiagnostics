5e67258e70389b9a9b246fdfe6b56f6f4248249f
Gowtham Rao <rao@ohdsi.org>
2021-06-01 13:44:46 -0400

    add error check in resolvedConceptSet in diagnostics explorer

--- inst/shiny/DiagnosticsExplorer/server.R
+++ inst/shiny/DiagnosticsExplorer/server.R
@@ -3274,7 +3274,9 @@ shiny::shinyServer(function(input, output, session) {
       databaseIds = input$database,
       timeIds = timeIds(),
       isTemporal = TRUE
-    ) %>%
+    )
+    if (is.null(data)) {return(data)}
+    data <- data %>%
       dplyr::select(-.data$choices) %>% 
       dplyr::inner_join(temporalCovariateChoices, by = "timeId") %>%
       dplyr::arrange(.data$timeId) %>%
