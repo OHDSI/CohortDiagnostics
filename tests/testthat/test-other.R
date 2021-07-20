55f4277bd8051f3e583112e9c7c8946a821972fa
Gowtham Rao <rao@ohdsi.org>
2021-07-20 06:59:52 -0400

    Added test for file encoding

--- tests/testthat/test-other.R
+++ tests/testthat/test-other.R
@@ -1,20 +1,32 @@
-test_that("Check if package is installed", {
+
+testthat::test_that("Check if package is installed", {
   testthat::expect_true(CohortDiagnostics:::is_installed('dplyr'))
   testthat::expect_false(CohortDiagnostics:::is_installed('rqrwqrewrqwRANDOMSTRINGdfdsfdsfds')) # just a random string to represent package does not exst
 })
 
-test_that("Check helper functions", {
-  testthat::expect_equal(CohortDiagnostics:::camelCaseToTitleCase('appleTree'), 'Apple Tree')
-  testthat::expect_equal(CohortDiagnostics:::snakeCaseToCamelCase('apple_tree'), 'appleTree')
+testthat::test_that("Check helper functions", {
+  testthat::expect_equal(CohortDiagnostics:::camelCaseToTitleCase('appleTree'),
+                         'Apple Tree')
+  testthat::expect_equal(CohortDiagnostics:::snakeCaseToCamelCase('apple_tree'),
+                         'appleTree')
   testthat::expect_equal(CohortDiagnostics:::quoteLiterals(NULL), '')
 })
 
-test_that("Ensure Cohort Diagnostics is installed", {
+testthat::test_that("Ensure Cohort Diagnostics is installed", {
   testthat::expect_null(CohortDiagnostics:::ensure_installed('CohortDiagnostics'))
 })
 
 
-test_that("util functions", {
-  expect_true(CohortDiagnostics:::naToEmpty(NA) == "")
-  expect_true(CohortDiagnostics:::naToZero(NA) == 0)
+testthat::test_that("util functions", {
+  testthat::expect_true(CohortDiagnostics:::naToEmpty(NA) == "")
+  testthat::expect_true(CohortDiagnostics:::naToZero(NA) == 0)
+})
+
+testthat::test_that("Test file encoding - using latin", {
+  # Latin encoding
+  tempfileLatin <- tempfile()
+  latinEncoding <- "fa\xE7ile"
+  writeChar(object = latinEncoding, con = tempfileLatin)
+  testthat::expect_error(CohortDiagnostics:::checkInputFileEncoding(fileName = tempfileLatin))
+  unlink(tempfileLatin)
 })
