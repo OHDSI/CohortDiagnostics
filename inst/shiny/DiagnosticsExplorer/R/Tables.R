cdc0ab474089c8e585b4606478aaba60581f0dd8
Gowtham Rao <rao@ohdsi.org>
2021-11-09 06:37:54 -0500

    G - comment out prepareTable1Comp

--- inst/shiny/DiagnosticsExplorer/R/Tables.R
+++ inst/shiny/DiagnosticsExplorer/R/Tables.R
@@ -161,132 +161,136 @@ prepareTable1 <- function(covariates,
 }
 
 
-#Prepare table for comparision ----
-prepareTable1Comp <- function(balance,
-                              pathToCsv = "Table1Specs.csv") {
-  balance <- balance %>%
-    dplyr::mutate(covariateName = stringr::str_to_sentence(
-      stringr::str_replace_all(
-        string = .data$covariateName,
-        pattern = "^.*: ",
-        replacement = ""
-      )
-    ))
-  space <- "&nbsp;"
-  specifications <- readr::read_csv(
-    file = pathToCsv,
-    col_types = readr::cols(),
-    guess_max = min(1e7)
-  ) %>%
-    dplyr::mutate(dplyr::across(
-      tidyr::everything(),
-      ~ tidyr::replace_na(data = .x, replace = '')
-    ))
-  
-  resultsTable <- tidyr::tibble()
-  
-  if (nrow(specifications) == 0) {
-    return(
-      dplyr::tibble(Note = 'There are no covariate records for the cohorts being compared.')
-    )
-  }
-  
-  for (i in 1:nrow(specifications)) {
-    specification <- specifications[i, ]
-    if (specification %>% dplyr::pull(.data$covariateIds) == "") {
-      balanceSubset <- balance %>%
-        dplyr::filter(.data$analysisId %in% specification$analysisId) %>%
-        dplyr::arrange(.data$covariateId)
-    } else {
-      balanceSubset <- balance %>%
-        dplyr::filter(
-          .data$analysisId %in% specification$analysisId,
-          .data$covariateId %in% (
-            stringr::str_split(
-              string = (specification %>%
-                          dplyr::pull(.data$covariateIds)),
-              pattern = ";"
-            )[[1]] %>%
-              utils::type.convert(as.is = TRUE)
-          )
-        ) %>%
-        dplyr::arrange(.data$covariateId)
-    }
-    
-    if (nrow(balanceSubset) > 0) {
-      resultsTable <- dplyr::bind_rows(
-        resultsTable,
-        tidyr::tibble(
-          characteristic = paste0(
-            '<strong>',
-            specification %>% dplyr::pull(.data$label),
-            '</strong>'
-          ),
-          MeanT = NA,
-          MeanC = NA,
-          StdDiff = NA,
-          header = 1,
-          position = i
-        ),
-        tidyr::tibble(
-          cohortId1 = balanceSubset$cohortId1,
-          cohortId2 = balanceSubset$cohortId2,
-          characteristic = paste0(space,
-                                  space,
-                                  space,
-                                  space,
-                                  balanceSubset$covariateName),
-          MeanT = balanceSubset$mean1,
-          MeanC = balanceSubset$mean2,
-          databaseId = balanceSubset$databaseId,
-          StdDiff = balanceSubset$absStdDiff,
-          header = 0,
-          position = i
-        )
-      ) %>%
-        dplyr::distinct() %>%
-        dplyr::mutate(sortOrder = dplyr::row_number())
-    }
-  }
-  if (nrow(resultsTable) > 0) {
-    resultsTable <- resultsTable %>%
-      dplyr::arrange(.data$position,
-                     dplyr::desc(.data$header),
-                     .data$sortOrder) %>%
-      dplyr::mutate(sortOrder = dplyr::row_number()) %>%
-      dplyr::select(-.data$header, -.data$position) %>%
-      dplyr::distinct()
-  }
-  
-  resultsTable <- resultsTable %>%
-    dplyr::mutate(
-      characteristic = stringr::str_replace(
-        string = .data$characteristic,
-        pattern = "black or african american",
-        replacement = "Black or African American"
-      )
-    ) %>%
-    dplyr::mutate(
-      characteristic = stringr::str_replace(
-        string = .data$characteristic,
-        pattern = "white",
-        replacement = "White"
-      )
-    ) %>%
-    dplyr::mutate(
-      characteristic = stringr::str_replace(
-        string = .data$characteristic,
-        pattern = "asian",
-        replacement = "Asian"
-      )
-    )
-  
-  return(resultsTable)
-}
+# #Prepare table for comparision ----
+# prepareTable1Comp <- function(balance,
+#                               pathToCsv = "Table1Specs.csv") {
+#   balance <- balance %>%
+#     dplyr::mutate(covariateName = stringr::str_to_sentence(
+#       stringr::str_replace_all(
+#         string = .data$covariateName,
+#         pattern = "^.*: ",
+#         replacement = ""
+#       )
+#     ))
+#   space <- "&nbsp;"
+#   specifications <- readr::read_csv(
+#     file = pathToCsv,
+#     col_types = readr::cols(),
+#     guess_max = min(1e7)
+#   ) %>%
+#     dplyr::mutate(dplyr::across(
+#       tidyr::everything(),
+#       ~ tidyr::replace_na(data = .x, replace = '')
+#     ))
+#   
+#   resultsTable <- tidyr::tibble()
+#   
+#   if (nrow(specifications) == 0) {
+#     return(
+#       dplyr::tibble(Note = 'There are no covariate records for the cohorts being compared.')
+#     )
+#   }
+#   
+#   for (i in 1:nrow(specifications)) {
+#     specification <- specifications[i, ]
+#     if (specification %>% dplyr::pull(.data$covariateIds) == "") {
+#       balanceSubset <- balance %>%
+#         dplyr::filter(.data$analysisId %in% specification$analysisId) %>%
+#         dplyr::arrange(.data$covariateId)
+#     } else {
+#       balanceSubset <- balance %>%
+#         dplyr::filter(
+#           .data$analysisId %in% specification$analysisId,
+#           .data$covariateId %in% (
+#             stringr::str_split(
+#               string = (specification %>%
+#                           dplyr::pull(.data$covariateIds)),
+#               pattern = ";"
+#             )[[1]] %>%
+#               utils::type.convert(as.is = TRUE)
+#           )
+#         ) %>%
+#         dplyr::arrange(.data$covariateId)
+#     }
+#     
+#     if (nrow(balanceSubset) > 0) {
+#       resultsTable <- dplyr::bind_rows(
+#         resultsTable,
+#         tidyr::tibble(
+#           characteristic = paste0(
+#             '<strong>',
+#             specification %>% dplyr::pull(.data$label),
+#             '</strong>'
+#           ),
+#           MeanT = NA,
+#           MeanC = NA,
+#           StdDiff = NA,
+#           header = 1,
+#           position = i
+#         ),
+#         tidyr::tibble(
+#           cohortId1 = balanceSubset$cohortId1,
+#           cohortId2 = balanceSubset$cohortId2,
+#           characteristic = paste0(space,
+#                                   space,
+#                                   space,
+#                                   space,
+#                                   balanceSubset$covariateName),
+#           MeanT = balanceSubset$mean1,
+#           MeanC = balanceSubset$mean2,
+#           databaseId = balanceSubset$databaseId,
+#           StdDiff = balanceSubset$absStdDiff,
+#           header = 0,
+#           position = i
+#         )
+#       ) %>%
+#         dplyr::distinct() %>%
+#         dplyr::mutate(sortOrder = dplyr::row_number())
+#     }
+#   }
+#   if (nrow(resultsTable) > 0) {
+#     resultsTable <- resultsTable %>%
+#       dplyr::arrange(.data$position,
+#                      dplyr::desc(.data$header),
+#                      .data$sortOrder) %>%
+#       dplyr::mutate(sortOrder = dplyr::row_number()) %>%
+#       dplyr::select(-.data$header, -.data$position) %>%
+#       dplyr::distinct()
+#   }
+#   
+#   resultsTable <- resultsTable %>%
+#     dplyr::mutate(
+#       characteristic = stringr::str_replace(
+#         string = .data$characteristic,
+#         pattern = "black or african american",
+#         replacement = "Black or African American"
+#       )
+#     ) %>%
+#     dplyr::mutate(
+#       characteristic = stringr::str_replace(
+#         string = .data$characteristic,
+#         pattern = "white",
+#         replacement = "White"
+#       )
+#     ) %>%
+#     dplyr::mutate(
+#       characteristic = stringr::str_replace(
+#         string = .data$characteristic,
+#         pattern = "asian",
+#         replacement = "Asian"
+#       )
+#     )
+#   
+#   return(resultsTable)
+# }
+# 
 
 # Compare cohort characteristics ----
 compareCohortCharacteristics <-
-  function(characteristics1, characteristics2) {
+  function(characteristics1, 
+           characteristics2,
+           value,
+           joinBy) {
     m <- dplyr::full_join(
       x = characteristics1 %>% dplyr::distinct(),
       y = characteristics2 %>% dplyr::distinct(),
