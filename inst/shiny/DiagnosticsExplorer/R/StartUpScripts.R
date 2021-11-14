cea9cfb575fafa6173310f54ea0bca1e405363d7
Gowtham Rao <rao@ohdsi.org>
2021-11-14 12:42:40 -0500

    G - retain the order of database in sketch function

--- inst/shiny/DiagnosticsExplorer/R/StartUpScripts.R
+++ inst/shiny/DiagnosticsExplorer/R/StartUpScripts.R
@@ -598,13 +598,21 @@ getDtWithColumnsGroupedByDatabaseId <- function(data,
                       minimumCellCountDefs) #!!!!!!!!!!! note in percent form, this may cause error because of -ve number
   )
 
+  databaseColumnNamesInRightOrder <-
+    colnames(data)[stringr::str_detect(
+      string = colnames(data),
+      pattern = paste0(keyColumns, collapse = "|"),
+      negate = TRUE
+    )] %>%
+    stringr::word(start = 1)
+  
   sketch <- htmltools::withTags(table(class = "display",
                                       thead(tr(
                                         lapply(camelCaseToTitleCase(keyColumns),
                                                th,
                                                rowspan = rowSpan),
                                         lapply(
-                                          databaseIdsForUseAsHeader %>% sort(),
+                                          databaseColumnNamesInRightOrder,
                                           th,
                                           colspan = numberOfSubstitutableColums,
                                           class = "dt-center",
