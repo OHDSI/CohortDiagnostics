6fd58bed4fa32d79f4f6ad4d0685d3b50e394166
Gowtham Rao <rao@ohdsi.org>
2022-02-08 22:02:00 -0500

    Add unit test

--- tests/testthat/test-other.R
+++ tests/testthat/test-other.R
@@ -3,3 +3,38 @@ test_that("Check if package is installed", {
   expect_true(CohortDiagnostics:::is_installed('dplyr'))
   expect_false(CohortDiagnostics:::is_installed('abcd'))
 })
+
+
+#check makeDataExportable function
+test_that("Check function makeDataExportable", {
+  cohortCountTableCorrect <- dplyr::tibble(
+    cohortId = 3423,
+    cohortEntries = 432432,
+    cohortSubjects = 34234,
+    databaseId = "ccae"
+  )
+  cohortCountTableCorrect <-
+    CohortDiagnostics:::makeDataExportable(x = cohortCountTableCorrect,
+                                           tableName = "cohort_count")
+  cohortCountTableCorrectNames <-
+    SqlRender::camelCaseToSnakeCase(names(cohortCountTableCorrect)) %>% sort()
+  
+  resultsDataModel <- getResultsDataModelSpecifications() %>%
+    dplyr::filter(.data$tableName == "cohort_count") %>%
+    dplyr::select(.data$fieldName) %>%
+    dplyr::pull() %>%
+    sort()
+  
+  expect_true(identical(cohortCountTableCorrectNames, resultsDataModel))
+  
+  cohortCountTableInCorrect <- dplyr::tibble(
+    cohortIdXXX = 3423,
+    cohortEntryXXX = 432432,
+    cohortSubjectsXXX = 34234
+  )
+  
+  expect_error(
+    CohortDiagnostics:::makeDataExportable(x = cohortCountTableInCorrect,
+                                           tableName = "cohort_count")
+  )
+})
