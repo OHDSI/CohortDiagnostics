bd7f3b71d6cd69aacd7321fe9fc2255651a67d60
Gowtham Rao <rao@ohdsi.org>
2022-03-14 18:11:59 -0400

    Update TestOtherPlatforms.R

--- extras/TestOtherPlatforms.R
+++ extras/TestOtherPlatforms.R
@@ -1,65 +1,112 @@
-# Tests for platforms not available to Travis
+# Performing tests outside github
 library(CohortDiagnostics)
+library(CohortGenerator)
+library(SkeletonCohortDiagnostics)
 library(testthat)
 
+
+
 # Settings -----------------------------------------------------------
-
-# BigQuery
-connectionDetails <- createConnectionDetails(dbms = "bigquery",
-                                             connectionString = keyring::key_get("bigQueryConnString"),
-                                             user = "",
-                                             password = "")
-
-cdmDatabaseSchema <- "synpuf_2m"
-cohortDatabaseSchema <- "synpuf_2m_results"
+cdmDatabaseSchema <- "CDMV5"
+cohortDatabaseSchema <- "OHDSI"
 cohortTable <- "cohortdiagnostics_test"
-folder <- "s:/temp"
+rootFolder <- file.path("d:", "temp", "test")
+outputFolder <- file.path(rootFolder, "results")
+incrementalFolder <- file.path(rootFolder, "incremental")
+cohortTable <- "cohort_test"
+
+
+cohortTableNames <- CohortGenerator::getCohortTableNames(cohortTable = cohortTable)
+# get cohort definitions from study package
+cohortDefinitionSet <-
+  dplyr::tibble(CohortGenerator::getCohortDefinitionSet(packageName = "SkeletonCohortDiagnosticsStudy", 
+                                                        cohortFileNameValue = "cohortId"))
+
+# BigQuery ---------------------------------
+# connectionDetails <- createConnectionDetails(dbms = "bigquery",
+#                                              connectionString = keyring::key_get("bigQueryConnString"),
+#                                              user = "",
+#                                              password = "")
+# tempEmulationSchema <- NULL
+
+# cdmDatabaseSchema <- "synpuf_2m"
+# cohortDatabaseSchema <- "synpuf_2m_results"
 
 # Drop temp tables not cleaned up:
 # connection = connect(connectionDetails)
 # dropEmulatedTempTables(connection = connection, tempEmulationSchema = cohortDatabaseSchema)
 # disconnect(connection)
 
-# RedShift 
-connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "redshift",
-                                                                connectionString = keyring::key_get("redShiftConnectionStringOhdaMdcd"),
-                                                                user = keyring::key_get("redShiftUserName"),
-                                                                password = keyring::key_get("redShiftPassword"))
+# RedShift  ---------------------------------
+# connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "redshift",
+#                                                                 connectionString = keyring::key_get("redShiftConnectionStringOhdaMdcd"),
+#                                                                 user = keyring::key_get("redShiftUserName"),
+#                                                                 password = keyring::key_get("redShiftPassword"))
+# tempEmulationSchema <- NULL
 
-cdmDatabaseSchema <- "cdm_truven_mdcd_v1734"
-cohortDatabaseSchema <- "scratch_mschuemi"
-cohortTable <- "cohortdiagnostics_test"
-folder <- "s:/temp/RS"
+# cdmDatabaseSchema <- "cdm_truven_mdcd_v1734"
+# cohortDatabaseSchema <- "scratch_mschuemi"
+# cohortTable <- "cohortdiagnostics_test"
+# folder <- "s:/temp/RS"
 
+
+# Oracle test ---------------------------------
+connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "oracle",
+                                                                server = Sys.getenv("CDM5_ORACLE_SERVER"),
+                                                                user = Sys.getenv("CDM5_ORACLE_USER"),
+                                                                password = Sys.getenv("CDM5_ORACLE_PASSWORD"))
+tempEmulationSchema <- Sys.getenv("CDM5_ORACLE_OHDSI_SCHEMA")
+
+# postgres test ---------------------------------
+# connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "oracle",
+#                                                                 server = Sys.getenv("CDM5_POSTGRESQL_SERVER"),
+#                                                                 user = Sys.getenv("CDM5_POSTGRESQL_USER"),
+#                                                                 password = Sys.getenv("CDM5_POSTGRESQL_PASSWORD"))
+# tempEmulationSchema <- NULL
+
+# SqlServer test ---------------------------------
+# connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "oracle",
+#                                                                 server = Sys.getenv("CDM5_SQL_SERVER_SERVER"),
+#                                                                 user = Sys.getenv("CDM5_SQL_SERVER_USER"),
+#                                                                 password = Sys.getenv("CDM5_SQL_SERVER_PASSWORD"))
+# cdmDatabaseSchema = 'cdmv5.dbo'
+# cohortDatabaseSchema = 'ohdsi.dbo'
+# tempEmulationSchema <- NULL
 
 # Cohort generation using CohortDiagnostics' instantiateCohortSet function -------------------------------
 
 test_that("Cohort instantiation", {
-  instantiateCohortSet(
+  
+  # Next create the tables on the database
+  CohortGenerator::createCohortTables(
     connectionDetails = connectionDetails,
-    cdmDatabaseSchema = cdmDatabaseSchema,
-    tempEmulationSchema = cohortDatabaseSchema,
+    cohortTableNames = cohortTableNames,
     cohortDatabaseSchema = cohortDatabaseSchema,
-    cohortTable = cohortTable,
-    packageName = "CohortDiagnostics",
-    cohortToCreateFile = "settings/CohortsToCreateForTesting.csv",
-    generateInclusionStats = TRUE,
-    createCohortTable = TRUE,
-    inclusionStatisticsFolder = file.path(folder, "incStats")
+    incremental = TRUE
   )
   
+  # Generate the cohort set
+  CohortGenerator::generateCohortSet(
+    connectionDetails = connectionDetails,
+    cdmDatabaseSchema = cdmDatabaseSchema,
+    cohortDatabaseSchema = cohortDatabaseSchema,
+    cohortTableNames = cohortTableNames,
+    cohortDefinitionSet = cohortDefinitionSet,
+    incrementalFolder = incrementalFolder,
+    incremental = TRUE
+  )
   
   connection <- DatabaseConnector::connect(connectionDetails)
   sql <-
     "SELECT COUNT(*) AS cohort_count, cohort_definition_id
-  FROM @cohort_database_schema.@cohort_table
-  GROUP BY cohort_definition_id;"
+      FROM @cohort_database_schema.@cohort_table
+      GROUP BY cohort_definition_id;"
   counts <-
     DatabaseConnector::renderTranslateQuerySql(
       connection,
       sql,
       cohort_database_schema = cohortDatabaseSchema,
-      cohort_table = cohortTable,
+      cohort_table = cohortTableNames$cohortTable,
       snakeCaseToCamelCase = TRUE
     )
   testthat::expect_gt(nrow(counts), 2)
@@ -67,28 +114,76 @@ test_that("Cohort instantiation", {
 })
 
 
-# Cohort instantiation using CohortGenerator -----------------------------------------------------------
-cohortDefinitionSet <- CohortGenerator::getCohortDefinitionSetFromPackage(packageName = "CohortDiagnostics",
-                                                                          fileName = "settings/CohortsToCreateForTesting.csv")
-cohortDefinitionSet <- cohortDefinitionSet[1:2, ] # Two cohorts is enough to test everything
-tableNames <- CohortGenerator::getCohortTableNames(cohortTable = cohortTable)
-CohortGenerator::createCohortTables(connectionDetails = connectionDetails,
-                                    cohortDatabaseSchema = cohortDatabaseSchema,
-                                    cohortTableNames = tableNames)
-CohortGenerator::generateCohortSet(connectionDetails = connectionDetails,
-                                   cdmDatabaseSchema = cdmDatabaseSchema,
-                                   tempEmulationSchema = cohortDatabaseSchema,
-                                   cohortDatabaseSchema = cohortDatabaseSchema,
-                                   cohortTableNames = tableNames,
-                                   cohortDefinitionSet = cohortDefinitionSet)
+
+test_that("Concept set diagnostics - with cohort table but not instantiated", {
+  
+  cohortTableNames <- CohortGenerator::getCohortTableNames(cohortTable = cohortTable)
+  
+  conceptSetDiagnostics <-
+    runConceptSetDiagnostics(
+      connectionDetails = connectionDetails,
+      tempEmulationSchema = tempEmulationSchema,
+      cdmDatabaseSchema = cdmDatabaseSchema,
+      vocabularyDatabaseSchema = cdmDatabaseSchema,
+      cohorts = cohortDefinitionSet,
+      cohortIds = NULL,
+      cohortDatabaseSchema = cohortDatabaseSchema,
+      cohortTable = cohortTableNames$cohortTable,
+      keep2BillionConceptId = TRUE,
+      runConceptSetOptimization = TRUE,
+      runExcludedConceptSet = TRUE,
+      runOrphanConcepts = TRUE,
+      runBreakdownIndexEvents = TRUE,
+      runBreakdownIndexEventRelativeDays = c(-5:5),
+      runIndexDateConceptCoOccurrence = TRUE,
+      runStandardToSourceMappingCount = TRUE,
+      runConceptCount = TRUE,
+      runConceptCountByCalendarPeriod = TRUE,
+      minCellCount = 0
+    )
+  
+  testthat::expect_true(object = Andromeda::isAndromeda(conceptSetDiagnostics))
+  
+})
+
 
 # Test CohortDiagnostics -------------------------------------------------------------------------
 test_that("Cohort diagnostics", {
   
-  cohortDefinitionSet$atlasId <- cohortDefinitionSet$cohortId
+  CohortDiagnostics::executeDiagnostics(packageName = "CohortDiagnostics",
+                                        cohortToCreateFile = "settings/CohortsToCreateForTesting.csv",
+                                        connectionDetails = connectionDetails,
+                                        cdmDatabaseSchema = cdmDatabaseSchema,
+                                        cohortDatabaseSchema = cohortDatabaseSchema,
+                                        cohortTable = cohortTable,
+                                        databaseId = "Synpuf",
+                                        exportFolder =  file.path(folder, "export"))
   
   
-  runCohortDiagnostics(packageName = "CohortDiagnostics",
+  executeDiagnostics(
+    cohortDefinitionSet = cohortDefinitionSet,
+    connectionDetails = connectionDetails,
+    cdmDatabaseSchema = cdmDatabaseSchema,
+    vocabularyDatabaseSchema = cdmDatabaseSchema,
+    tempEmulationSchema = cohortDatabaseSchema,
+    cohortDatabaseSchema = cohortDatabaseSchema,
+    cohortTable = cohortTable,
+    inclusionStatisticsFolder = file.path(folder, "incStats"),
+    exportFolder =  file.path(folder, "export"),
+    databaseId = "SynPuf",
+    minCellCount = 5
+  )
+  
+  
+  testthat::expect_true(file.exists(file.path(
+    folder, "export", "Results_SynPuf.zip"
+  )))
+})
+
+# Test CohortDiagnostics -------------------------------------------------------------------------
+test_that("Cohort diagnostics", {
+
+  CohortDiagnostics::executeDiagnostics(packageName = "CohortDiagnostics",
                        cohortToCreateFile = "settings/CohortsToCreateForTesting.csv",
                        connectionDetails = connectionDetails,
                        cdmDatabaseSchema = cdmDatabaseSchema,
