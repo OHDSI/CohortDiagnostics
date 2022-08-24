a20cb6525c8c167819e219733a456679a754054d
Gowtham Rao <rao@ohdsi.org>
2022-08-24 17:05:50 -0400

    Maybe changing the indentation might fix it

--- R/RunDiagnostics.R
+++ R/RunDiagnostics.R
@@ -129,9 +129,9 @@ getDefaultCovariateSettings <- function() {
 #'                                    the createTemporalCovariateSettings function in the FeatureExtraction package, or a list
 #'                                    of such objects.
 #' @param minCellCount                The minimum cell count for fields contains person counts or fractions.
-#' @param minCharacterizationMean     The minimum mean value for characterization output. Values below this will be cut off from output.
-#'                                    This will help reduce the file size of the characterization output, but will remove information
-#'                                    on covaraiates that have very low values. The default is 0.001 (i.e. 0.1%)
+#' @param minCharacterizationMean     The minimum mean value for characterization output. Values below this will be cut off from output. This 
+#'                                    will help reduce the file size of the characterization output, but will remove information
+#'                                    on covariates that have very low values. The default is 0.001 (i.e. 0.1%)
 #' @param incremental                 Create only cohort diagnostics that haven't been created before?
 #' @param incrementalFolder           If \code{incremental = TRUE}, specify a folder where records are kept
 #'                                    of which cohort diagnostics has been executed.
