7095c29b775fffc0796222208de382d6259a20a7
Gowtham Rao <rao@ohdsi.org>
2022-08-24 17:09:56 -0400

    Looks like it doesnt like percent sign

--- R/RunDiagnostics.R
+++ R/RunDiagnostics.R
@@ -124,14 +124,14 @@ getDefaultCovariateSettings <- function() {
 #' @param runCohortRelationship       Generate and export the cohort relationship? Cohort relationship checks the temporal
 #'                                    relationship between two or more cohorts.
 #' @param runTemporalCohortCharacterization   Generate and export the temporal cohort characterization?
-#'                                    Only records with values greater than 0.001 are returned.
+#'                                            Only records with values greater than 0.001 are returned.
 #' @param temporalCovariateSettings   Either an object of type \code{covariateSettings} as created using one of
 #'                                    the createTemporalCovariateSettings function in the FeatureExtraction package, or a list
 #'                                    of such objects.
 #' @param minCellCount                The minimum cell count for fields contains person counts or fractions.
 #' @param minCharacterizationMean     The minimum mean value for characterization output. Values below this will be cut off from output. This 
 #'                                    will help reduce the file size of the characterization output, but will remove information
-#'                                    on covariates that have very low values. The default is 0.001 (i.e. 0.1%)
+#'                                    on covariates that have very low values. The default is 0.001 (i.e. 0.1 percent)
 #' @param incremental                 Create only cohort diagnostics that haven't been created before?
 #' @param incrementalFolder           If \code{incremental = TRUE}, specify a folder where records are kept
 #'                                    of which cohort diagnostics has been executed.
