aeae3577a345c372b217f12c1ffe2ba359efbcdb
Gowtham Rao <rao@ohdsi.org>
2022-08-27 06:18:13 -0400

    Use temp table instead of CTEs (for optimization)

--- R/TimeSeries.R
+++ R/TimeSeries.R
@@ -215,41 +215,47 @@ runCohortTimeSeriesDiagnostics <- function(connectionDetails = NULL,
     progressBar = FALSE,
     reportOverallTime = FALSE
   )
-  sqlCohort <- "--HINT DISTRIBUTE_ON_KEY(subject_id)
-                WITH cohort
-                AS (
-                	SELECT *
-                	FROM @cohort_database_schema.@cohort_table
-                  {@cohort_ids != ''} ? { where cohort_definition_id IN (@cohort_ids)}
-                	),
-                cohort_first
-                AS (
-                	SELECT cohort_definition_id,
-                		subject_id,
-                		min(cohort_start_date) cohort_start_date,
-                		min(cohort_end_date) cohort_end_date
-                	FROM cohort
-                	GROUP BY cohort_definition_id,
-                		subject_id
-                	)
+  sqlCohort <- "DROP TABLE IF EXISTS #ts_cohort;
+                DROP TABLE IF EXISTS #ts_cohort_first;
+                DROP TABLE IF EXISTS #ts_output;
+                
+                --HINT DISTRIBUTE_ON_KEY(subject_id)
+                SELECT *
+                INTO #ts_cohort
+                FROM @cohort_database_schema.@cohort_table {@cohort_ids != '' } ? {
+                WHERE cohort_definition_id IN (@cohort_ids) };
+                
+                --HINT DISTRIBUTE_ON_KEY(subject_id)
+                SELECT cohort_definition_id,
+                	subject_id,
+                	min(cohort_start_date) cohort_start_date,
+                	min(cohort_end_date) cohort_end_date
+                INTO #ts_cohort_first
+                FROM #ts_cohort
+                GROUP BY cohort_definition_id,
+                	subject_id;
+                
+                
                 SELECT c.*,
-                	CASE
+                	CASE 
                 		WHEN c.cohort_start_date = cf.cohort_start_date
                 			THEN 'Y'
                 		ELSE 'N'
-                		END first_occurrence
-                		{@stratify_by_gender} ? {, concept.concept_name gender}
-                		{@stratify_by_age_group} ? {, p.year_of_birth}
+                		END first_occurrence {@stratify_by_gender} ? {,
+                	concept.concept_name gender} {@stratify_by_age_group} ? {,
+                	p.year_of_birth}
                 INTO #cohort_ts
-                FROM cohort c
-                INNER JOIN cohort_first cf ON c.cohort_definition_id = cf.cohort_definition_id
-                	AND c.subject_id = cf.subject_id
-
-                {@stratify_by_gender  | @stratify_by_age_group} ? {
-                INNER JOIN @cdm_database_schema.person p ON c.subject_id = p.person_id}
-
-                {@stratify_by_gender} ? {
-                INNER JOIN @cdm_database_schema.concept ON p.gender_concept_id = concept.concept_id};"
+                FROM #ts_cohort c
+                INNER JOIN #ts_cohort_first cf
+                	ON c.cohort_definition_id = cf.cohort_definition_id
+                		AND c.subject_id = cf.subject_id {@stratify_by_gender | @stratify_by_age_group} ? {
+                INNER JOIN @cdm_database_schema.person p
+                	ON c.subject_id = p.person_id} {@stratify_by_gender} ? {
+                INNER JOIN @cdm_database_schema.concept
+                	ON p.gender_concept_id = concept.concept_id};
+  
+                DROP TABLE IF EXISTS #ts_cohort;
+                DROP TABLE IF EXISTS #ts_cohort_first;"
 
   ParallelLogger::logTrace("   - Creating cohort table copy for time series")
   DatabaseConnector::renderTranslateExecuteSql(
