7622f59a4502a0f4c8815b4137d6456ce6901db0
Jamie Gilbert <JGilber2@its.jnj.com>
2022-12-06 13:30:56 -0800

    package maintenance

--- inst/sql/sql_server/UpdateVersionNumber.sql
+++ inst/sql/sql_server/UpdateVersionNumber.sql
@@ -1,5 +1,6 @@
 {DEFAULT @package_version = package_version}
-{DEFAULT @version_number = '3.1.0'}
+{DEFAULT @version_number = '3.1.1'}
 
 DELETE FROM @database_schema.@table_prefix@package_version;
 INSERT INTO @database_schema.@table_prefix@package_version (version_number) VALUES ('@version_number');
+
