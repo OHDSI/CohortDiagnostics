bbe4a6a8ab6971c5abc0e0c53dba531fb3d65db6
Cesar Barboza <c.barbozagutierrez@erasmusmc.nl>
2023-08-03 13:09:07 +0200

    change sql

--- inst/sql/sql_server/CreateConceptCountTable.sql
+++ inst/sql/sql_server/CreateConceptCountTable.sql
@@ -105,11 +105,11 @@ FROM (
 
 ALTER TABLE @concept_counts_table
 ADD vocabulary_version VARCHAR(20) NULL;
-UPDATE @concept_counts_table SET vocabulary_version = (SELECT vocabulary_version FROM @cdm_database_schema.vocabulary WHERE vocabulary_id = "None");
+UPDATE @concept_counts_table SET vocabulary_version = (SELECT vocabulary_version FROM @cdm_database_schema.vocabulary WHERE vocabulary_id = 'None');
 } : { 
 ALTER TABLE @work_database_schema.@concept_counts_table
 ADD vocabulary_version VARCHAR(20) NULL;
-UPDATE @work_database_schema.@concept_counts_table SET vocabulary_version = (SELECT vocabulary_version FROM @cdm_database_schema.vocabulary WHERE vocabulary_id = "None");
+UPDATE @work_database_schema.@concept_counts_table SET vocabulary_version = (SELECT vocabulary_version FROM @cdm_database_schema.vocabulary WHERE vocabulary_id = 'None');
 }
 
 
