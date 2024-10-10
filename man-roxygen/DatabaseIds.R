b20a8343f8c92586b74483226f527f1a52513197
Ger Inberg <ginberg@gmail.com>
2024-10-10 10:35:52 +0200

    formatting

--- man-roxygen/DatabaseIds.R
+++ man-roxygen/DatabaseIds.R
@@ -3,4 +3,4 @@
 #'                              of the results data model.
 #' @param databaseId            The databaseId to retrieve the results for. This is a 
 #'                              character field values from the `databaseId` field of the `database` table 
-#'                              of the results data model.                             
+#'                              of the results data model.                    
