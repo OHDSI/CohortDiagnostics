74558b1012c736e62afe98f48ec649d6022cf5b2
jgilber2 <jgilber2@its.jnj.com>
2025-03-06 12:27:57 -0800

    Handle data migration

--- inst/sql/sql_server/migrations/Migration_6-v3_4_0_remove_cohort_relationships.sql
+++ inst/sql/sql_server/migrations/Migration_6-v3_4_0_remove_cohort_relationships.sql
@@ -1,8 +1,50 @@
 {DEFAULT @migration = migration}
 {DEFAULT @cohort_relationships = cohort_relationships}
+{DEFAULT @cohort_count = cohort_count}
+{DEFAULT @cohort = cohort}
+{DEFAULT @temporal_covariate_ref = temporal_covariate_ref}
+{DEFAULT @temporal_analysis_ref = temporal_analysis_ref}
+{DEFAULT @temporal_covariate_value = temporal_covariate_value}
 {DEFAULT @table_prefix = ''}
+{DEFAULT @analysis_id = 173} --TODO HARD CODING IS HARD TO SOLVE
 
--- TODO: Migration to FeatureExtraction output
+-- Migrate data to FeatureExtraction output
+INSERT INTO @database_schema.@table_prefix@temporal_covariate_ref
+SELECT DISTINCT
+       CONCAT(CAST(cr.cohort_id AS varchar), '@analysis_id') AS covariate_id,
+       CONCAT('cohort:', c.cohort_name) AS covariate_name,
+       @analysis_id as analysis_id,
+       0 as concept_id
+FROM @database_schema.@table_prefix@cohort_relationships cr
+INNER JOIN @database_schema.@table_prefix@cohort c ON cr.cohort_id = c.cohort_id
+;
+
+INSERT INTO @database_schema.@table_prefix@temporal_analysis_ref
+        (analysis_id, analysis_name, domain_id, is_binary)
+SELECT
+    DISTINCT
+    @analysis_id as analysis_id,
+    'CohortTemporal' as analysis_name,
+    'cohort' as domain_id,
+    'Y' as is_binary
+FROM @database_schema.@table_prefix@cohort_relationships
+-- only do this if cohort_relationships contains data
+WHERE (SELECT COUNT(*) FROM @database_schema.@table_prefix@cohort_relationships) > 0;
+
+
+INSERT INTO @database_schema.@table_prefix@temporal_covariate_value
+SELECT
+    CONCAT(CAST(cr.comparator_cohort_id AS varchar), '@analysis_id') AS covariate_id,
+    cr.database_id,
+    cr.cohort_id,
+    cr.subjects as sum_value, -- total in both cohorts
+    cr.subjects / cc.cohort_subjects as mean,  -- fraction that overlap, can be used in characterization view
+    0 as sd, -- not sure how we get this value and it isn't used in the main report
+    1 as time_id
+FROM @database_schema.@table_prefix@cohort_relationships cr
+INNER JOIN @database_schema.@table_prefix@cohort_count cc on cr.cohort_id = cc.cohort_id
+WHERE start_day = -9999 AND end_day = 0 -- Until now, this is the only time window that has actually been used
+;
 
 -- Remove old table
 DROP TABLE IF EXISTS @database_schema.@table_prefix@cohort_relationships;
